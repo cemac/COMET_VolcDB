823b2068861e1b41516f0d477fa7a06edf677721
helen <h.l.burns@leeds.ac.uk>
2019-09-13 17:15:26 +0100

    :hammer_and_wrench: more db testing

--- FRONTEND/login.sql
+++ FRONTEND/login.sql
@@ -2,8 +2,10 @@ create table users_roles (
 id integer,
 group_id integer,
 PRIMARY KEY (id, group_id),
+CONSTRAINT user_id
 FOREIGN KEY (id) REFERENCES users (id)
 ON DELETE CASCADE ON UPDATE NO ACTION,
+CONSTRAINT role_id
 FOREIGN KEY (group_id) REFERENCES roles (group_id)
 ON DELETE CASCADE ON UPDATE NO ACTION
 );
