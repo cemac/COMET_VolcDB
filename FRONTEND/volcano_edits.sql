0ee4792c4edb0cf054308c041d4fe4b90addb0a0
helen <h.l.burns@leeds.ac.uk>
2020-01-23 11:09:35 +0000

    :card_file_box: prep for moderating volcanoes

--- FRONTEND/volcano_edits.sql
+++ FRONTEND/volcano_edits.sql
@@ -1,3 +1,6 @@
+alter table VolcDB1 add column "Review needed" VARCHAR;
+alter table VolcDB1 add column "date_edited" DATE;
+
 create table VolcDB1_edits (
 ID INTEGER PRIMARY KEY,
 characteristics_of_deformation TEXT,
@@ -11,10 +14,26 @@ longitude REAL  NOT NULL,
 measurement_methods TEXT,
 name TEXT NOT NULL,
 'references' TEXT,
+"frames" TEXT,
 volcano_number INTEGER,
 image_url TEXT,
 Area TEXT  NOT NULL,
 owner_id integer NOT NULL,
 FOREIGN KEY (owner_id) REFERENCES users (id)
 FOREIGN KEY (id) REFERENCES VolcDB1 (ID)
+);
+
+create table site_text (
+ID INTEGER PRIMARY KEY,
+current_words TEXT,
+old_words TEXT,
+type TEXT
+);
+
+create table images (
+ID INTEGER PRIMARY KEY,
+file TEXT,
+size TEXT,
+alttext TEXT,
+link TEXT
 );
