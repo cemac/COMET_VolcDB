19d99be4050250dfc19d9687e16ff232ed66ab67
cemachelen <h.l.burns@leeds.ac.uk>
2020-05-20 12:19:05 +0100

    :pencil2: fill dummy value for all "Nones"

--- FRONTEND/COMETApp.py
+++ FRONTEND/COMETApp.py
@@ -151,9 +151,8 @@ def volcano(country, region, volcano):
     if len(df.index) == 0:
         df = pd.read_sql_query("SELECT * FROM VolcDB1 WHERE " +
                                "ID = '" + str(volcano) + "';", conn)
-    # fill in missing region and country                           
-    df.Area.fillna(value='Unknown', inplace=True)
-    df.country.fillna(value='Unknown', inplace=True)
+    # fill in missing region and country
+    df.fillna(value='Unknown', inplace=True)
     # If there is a pending update
     pending = df['Review needed'].values
     editor = False
