106e68f01b9bbb04f122fc55e465bfec56c50afe
Richard Rigby <r.rigby@leeds.ac.uk>
2020-12-10 13:36:14 +0000

    Update links to LiCS products
    Track directories no longer have leading zeros ...

--- FRONTEND/static/js/s1_images.js
+++ FRONTEND/static/js/s1_images.js
@@ -47,40 +47,43 @@ function display_prob_image(index) {
   var links_pha = document.getElementById('s1_pha_data_links');
   var links_unw = document.getElementById('s1_unw_data_links');
 
+  /* track directory in links does not have leading zeros: */
+  var track_dir = parseInt(volcano_track);
+
   /* set data links: */
   var links_date = prob_data['dates'][image_index]
   links_date = links_date.replace(' - ', '_');
   links_date = links_date.replace(/-/g, '');
   links_cc.innerHTML = '<div class="div_data_links"><span>Coherence</span></div>' +
                        '<div class="div_data_links"><a href="' +
-                       data_href_prefix + '/' + volcano_track +
+                       data_href_prefix + '/' + track_dir +
                        '/' + volcano_frame + '/interferograms/' + links_date +
                        '/' + links_date + '.geo.cc.tif">' + links_date +
                        '.geo.cc.tif</a></div>' +
                        '<div class="div_data_links"><a href="' +
-                       data_href_prefix + '/' + volcano_track +
+                       data_href_prefix + '/' + track_dir +
                        '/' + volcano_frame + '/interferograms/' + links_date +   
                        '/' + links_date + '.geo.cc.png">' + links_date +
                        '.geo.cc.png</a></div>';
    links_pha.innerHTML = '<div class="div_data_links"><span>LOS change</span></div>' +
                          '<div class="div_data_links"><a href="' +
-                         data_href_prefix + '/' + volcano_track +
+                         data_href_prefix + '/' + track_dir +
                          '/' + volcano_frame + '/interferograms/' + links_date +   
                          '/' + links_date + '.geo.diff_pha.tif">' + links_date +
                          '.geo.diff_pha.tif</a></div>' +
                          '<div class="div_data_links"><a href="' +
-                         data_href_prefix + '/' + volcano_track +
+                         data_href_prefix + '/' + track_dir +
                          '/' + volcano_frame + '/interferograms/' + links_date +   
                          '/' + links_date + '.geo.diff.png">' + links_date +
                          '.geo.diff.png</a></div>';
    links_unw.innerHTML = '<div class="div_data_links"><span>Unwrapped LOS change</span></div>' +
                          '<div class="div_data_links"><a href="' +
-                         data_href_prefix + '/' + volcano_track +
+                         data_href_prefix + '/' + track_dir +
                          '/' + volcano_frame + '/interferograms/' + links_date +   
                          '/' + links_date + '.geo.unw.tif">' + links_date +
                          '.geo.unw.tif</a></div>' + 
                          '<div class="div_data_links"><a href="' +
-                         data_href_prefix + '/' + volcano_track +
+                         data_href_prefix + '/' + track_dir +
                          '/' + volcano_frame + '/interferograms/' + links_date +   
                          '/' + links_date + '.geo.unw.png">' + links_date +
                          '.geo.unw.png</a></div>';
