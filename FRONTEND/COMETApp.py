b6a16367f2ee2ac9cb587207288c65d7e80967aa
Richard Rigby <r.rigby@leeds.ac.uk>
2021-05-10 13:27:38 +0100

    Add frame to recent activity links
    If this works correctly, the links to recent activity should load
    the relevant frame, where activity was detected, when followed.

--- FRONTEND/COMETApp.py
+++ FRONTEND/COMETApp.py
@@ -77,6 +77,7 @@ def index():
     volcs = df.loc[df.jasmin_name.isin(recent_events.index.to_list())].sort_values(by=['jasmin_name'])
     recent_events = recent_events.loc[recent_events.index.isin(df.jasmin_name.to_list())]
     volcs['prob_date'] = recent_events.sort_index()['prob_date'].values
+    volcs['prob_frame'] = recent_events.sort_index()['prob_frame'].values
     total = len(volcs.index)
     return render_template('home.html.j2',
                            volcinfo=json.dumps(volcinfo.values.tolist()),
